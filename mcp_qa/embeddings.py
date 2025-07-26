4e15fb2cc6fbeced84992db5d7ef5c96d9d98768
wiliin <richohealthy@hotmail.com>
2025-07-27 04:54:44 +0800

    chore: 删除未使用的代码并更新.gitignore
    -删除不使用的Python文件：build_index.py、main.py、mcp_client.py、mcp_qa/*
    -更新.gitignore以排除日志文件

--- mcp_qa/embeddings.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-
-# 向量模型和检索器初始化
-embeddings = HuggingFaceEmbeddings(model_name="paraphrase-multilingual-MiniLM-L12-v2")
-vector_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-retriever = vector_db.as_retriever(search_kwargs={"k": 3}) 
